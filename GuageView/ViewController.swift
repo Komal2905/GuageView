03d8da4212022c2ee410138d72fd6a1f6364e9cd
komal kamble <komal@komals-MacBook-Air.local>
2017-10-01 18:07:34 +0530

    Initial Commit

---
+++ GuageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuageView
+//
+//  Created by komal kamble on 01/10/17.
+//  Copyright © 2017 komal kamble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
